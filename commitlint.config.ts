40b268af1b66ec534c4e20940c36b0a03dc1bf41
Alfin Noviaji <alpinnz@gmail.com>
2025-11-06 13:50:07 +0700

    chore: setup husky

---
+++ commitlint.config.ts
@@ -0,0 +1,27 @@
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "test",
+        "chore",
+        "build",
+        "ci",
+        "perf",
+        "revert",
+      ],
+    ],
+    "subject-case": [2, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
+  },
+};
+
+export default Configuration;
